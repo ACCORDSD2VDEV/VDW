01d7333668800969b6858a7ba0dd79790b2b33b8
CartWill <William.A.Carter@ucdenver.edu>
2017-06-20 10:15:09 -0600

    Updates

--- VDW3_1/Lookup Table Scripts/VDW_V3.1_TableDefs_ABN_IND_LU.sql
+++ VDW3_1/Lookup Table Scripts/VDW_V3.1_TableDefs_ABN_IND_LU.sql
@@ -1,30 +1,39 @@
 IF OBJECT_ID('CHORDS_VDW31_ABN_IND_LU') IS NOT NULL
 BEGIN
-	PRINT 'CHORDS_VDW31_ABN_IND_LU Already Exists';
+	 PRINT 'CHORDS_VDW31_ABN_IND_LU Already Exists';
 END;
 ELSE
 BEGIN
-	PRINT 'CHORDS_VDW31_ABN_IND_LU - Creating';
-	SET QUOTED_IDENTIFIER ON;
-	CREATE TABLE [CHORDS_VDW31_ABN_IND_LU]
-	( 
-				 [ABBREVIATION] nvarchar(2) NOT NULL, 
-				 [DESCRIPTION] nvarchar(50) NOT NULL, 
-				 CONSTRAINT [PK_CHORDS_VDW31_ABN_IND_LU] PRIMARY KEY CLUSTERED([ABBREVIATION] ASC)
-	)
-	ON [PRIMARY];
-	INSERT INTO [CHORDS_VDW31_ABN_IND_LU]( 
-		[ABBREVIATION], 
-		[DESCRIPTION] )
-	VALUES( 
-		   N'AB', N'abnormal' ), ( 
-		   N'C', N'critical' ), ( 
-		   N'AH', N'abnormal high' ), ( 
-		   N'CH', N'critical high' ), ( 
-		   N'AL', N'abnormal low' ), ( 
-		   N'CL', N'critical low' ), ( 
-		   N'IN', N'inconclusive' ), ( 
-		   N'UK', N'unknown' ), ( 
-		   N'NL', N'normal' );
-	PRINT 'CHORDS_VDW31_ABN_IND_LU - Complete';
+	 PRINT 'CHORDS_VDW31_ABN_IND_LU - Creating';
+	 SET QUOTED_IDENTIFIER ON;
+	 CREATE TABLE [CHORDS_VDW31_ABN_IND_LU]
+	 (
+					  [ABBREVIATION]    nvarchar(2) NOT NULL,
+					  [DESCRIPTION]     nvarchar(50) NOT NULL,
+					  CONSTRAINT [PK_CHORDS_VDW31_ABN_IND_LU] PRIMARY KEY CLUSTERED([ABBREVIATION] ASC)
+	 )
+	 ON [PRIMARY];
+	 INSERT INTO [CHORDS_VDW31_ABN_IND_LU](
+		  [ABBREVIATION],
+		  [DESCRIPTION] )
+	 VALUES (
+			  N'AB',
+			  N'abnormal' ), (
+			  N'C',
+			  N'critical' ), (
+			  N'AH',
+			  N'abnormal high' ), (
+			  N'CH',
+			  N'critical high' ), (
+			  N'AL',
+			  N'abnormal low' ), (
+			  N'CL',
+			  N'critical low' ), (
+			  N'IN',
+			  N'inconclusive' ), (
+			  N'UK',
+			  N'unknown' ), (
+			  N'NL',
+			  N'normal' );
+	 PRINT 'CHORDS_VDW31_ABN_IND_LU - Complete';
 END;
